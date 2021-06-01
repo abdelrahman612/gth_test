58cbb58dff088c90e44644bbbcd1b927e7dced06
abdelrahman612 <abdelrahman.elkaliby@gmail.com>
2021-06-01 12:38:58 +0200

    test third commit

--- lib/screen/screen_page.dart
+++ lib/screen/screen_page.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Mybody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Other();
+  }
+}
+
+class Other extends StatefulWidget {
+  @override
+  _OtherState createState() => _OtherState();
+}
+
+class _OtherState extends State<Other> {
+  @override
+  Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        child: Text('hi test'),
+      child: GestureDetector(
+        onTap: (){
+          print('ok');
+        },
+        child: Container(
+          child: Icon(Icons.exit_to_app),
+        ),
       ),
     );
   }

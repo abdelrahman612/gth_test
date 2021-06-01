c0e95eaa0110081356c5e2c082e57ab895bab874
abdelrahman612 <abdelrahman.elkaliby@gmail.com>
2021-06-01 12:33:06 +0200

    test secend commit

---
+++ lib/screen/screen_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class Mybody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text('hi test'),
+      ),
+    );
+  }
+}
